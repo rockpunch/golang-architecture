a25e8b66a71fe56c1f4592bc3aeb44489d2d5b8f
sehy0121 <sbae3@artic.edu>
2020-03-05 02:44:00 +0900

    fixed main function

--- cmd/person/main.go
+++ cmd/person/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/rockpunch/golang-architecture"
+	architecture "github.com/rockpunch/golang-architecture"
 	"github.com/rockpunch/golang-architecture/storage/mongo"
 	"github.com/rockpunch/golang-architecture/storage/postgrs"
 )
@@ -22,10 +22,10 @@ func main() {
 	ps := architecture.NewPersonService(dbm)
 
 	fmt.Printf("\nPrinting the dbm result\n\n")
-	architecture.Put(dbm, 1, p1)
-	architecture.Put(dbm, 2, p2)
-	fmt.Println(architecture.Get(dbm, 1))
-	fmt.Println(architecture.Get(dbm, 2))
+	architecture.PersonService.Put(dbm, 1, p1)
+	architecture.PersonService.Put(dbm, 2, p2)
+	fmt.Println(architecture.PersonService.Get(dbm, 1))
+	fmt.Println(architecture.PersonService.Get(dbm, 2))
 
 	fmt.Println(ps.Get(1))
 	fmt.Println(ps.Get(3))
@@ -34,9 +34,9 @@ func main() {
 
 	fmt.Printf("Printing the dbp result\n\n")
 
-	architecture.Put(dbp, 1, p1)
-	architecture.Put(dbp, 2, p2)
-	fmt.Println(architecture.Get(dbp, 1))
-	fmt.Println(architecture.Get(dbp, 2))
+	architecture.PersonService.Put(dbp, 1, p1)
+	architecture.PersonService.Put(dbp, 2, p2)
+	fmt.Println(architecture.PersonService.Get(dbp, 1))
+	fmt.Println(architecture.PersonService.Get(dbp, 2))
 }
 
--- storage/mongo/mongo.go
+++ storage/mongo/mongo.go
@@ -4,7 +4,7 @@ import "github.com/rockpunch/golang-architecture"
 
 type Db map[int] architecture.Person
 
-func (m Db) Save(n int, p architecture.Person) {a
+func (m Db) Save(n int, p architecture.Person) {
 	m[n] = p
 }
 func (m Db) Retrieve(n int) architecture.Person {
