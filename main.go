dbd480d5e619650893630dbcd78dbfa55e8fe2d5
sehy0121 <sbae3@artic.edu>
2020-03-05 01:20:17 +0900

    added additional examples

--- main.go
+++ main.go
@@ -7,8 +7,17 @@ type person struct {
 	first string
 }
 
+type secretAgent struct {
+	person
+	ltk bool
+}
+
 func (p person) speak() {
 	fmt.Printf("My name is %v\n", p.first)
+}
+
+func (sa secretAgent) speak() {
+	fmt.Printf("I'm a secret agent %v\n", sa.first)
 }
 
 //an interface says
@@ -23,11 +32,23 @@ func main() {
 	p1 := person{
 		first: "James",
 	}
-	fmt.Printf("p1 is a type of %T\nand it can be represented as: %v\n:)\n", p1, p1)
+
+	sa1 := secretAgent{
+		person: person{first:"Miss Moneypenny"},
+		ltk:    true,
+	}
+
+	fmt.Printf("p1 is a type of %T\nand it can be represented as: %v\n:)\n\n", p1, p1)
 
 	// in go a VALUE can be of more than one TYPE
 	// in this example p1 is both (concrete)TYPE person and (abstract)TYPE human
-	var h1 human
+	var h1, h2 human
 	h1 = p1
-	fmt.Printf("\nh1 is a type of %T\n", h1)
+	h2 = sa1
+
+	// we know that human carries speak method
+	h1.speak()
+
+	// sa has person. logically, it has to have two speak method. but what would happen?
+	h2.speak()
 }
